f6df2051ae59fd35f203d84263a58c829962c36a
only-pine <hansol302@gmail.com>
2025-01-22 11:47:16 +0900

    Time: 9 ms (24.3%), Space: 52.3 MB (58.8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,28 +2,44 @@
  * @param {string} s
  * @return {boolean}
  */
- /*
-- 빈칸과 알파벳이 아닌 문자들은 제거
- */
-var isPalindrome = function(s) {
-    const str = [];
-    for (const element of s.toLowerCase()) {
-        if ((element.charCodeAt(0) >= 97 && element.charCodeAt(0) <= 122)
-           || parseInt(element) >= 0) {
-            str.push(element);
-        }
-    }
+var isPalindrome = function (s) {
+    let phrase = s.toLowerCase().trim();
+    let start = 0, end = phrase.length - 1;
 
-    const reverseStr = str.slice().reverse();
-    
-    let index = 0;
-    while (str.length >= index) {
-        if (str[index] !== reverseStr[index]) {
+    var checkAlphanumericChar = function(word) {
+        if((
+            word.charCodeAt(0) >= 97 && word.charCodeAt(0) <= 122) || 
+            (Number(word) >= 0 && Number(word) <= 9
+            ) && word != " "
+        ) {
+            return true;
+        } else {
             return false;
         }
-
-        index += 1;
     }
-    
+
+    while (start < phrase.length) {
+        if (checkAlphanumericChar(phrase[start])) {
+            if (checkAlphanumericChar(phrase[end])) {
+                if (phrase[start] === phrase[end]) {
+                    start++;
+                    end--;
+                } else {
+                    return false;
+                }
+            } else {
+                end--;
+            }
+        } else {
+            start++;
+        }
+    }
+
     return true;
 };
+
+/*
+    1. 전달받은 문구를 소문자로 변환시킨다.
+    2. 숫자를 담는 변수 2개를 선언하고, 하나는 0, 나머지 하나는 문구의 길이 - 1을 할당한다.
+    3. 반복문을 통해서 문구의 앞뒤가 동일한 알파벳인지 확인한다.
+*/

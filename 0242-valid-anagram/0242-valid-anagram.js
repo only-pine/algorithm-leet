2add6f2751d2c62e729de9e8407614d0c66a10fa
only-pine <hansol302@gmail.com>
2025-01-24 15:45:43 +0900

    Time: 15 ms (64.96%), Space: 53.7 MB (45.27%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const map = new Map();
+
+    for (const char of s) {
+        map.set(char, map.get(char) ? map.get(char) + 1 : 1);
+    }
+
+    for (const char of t) {
+        if (map.get(char) === undefined || map.get(char) === 0) {
+            return false;
+        }
+
+        map.set(char, map.get(char) - 1);
+    }
+
+    return true;
+};

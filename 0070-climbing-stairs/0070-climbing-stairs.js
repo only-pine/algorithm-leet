7f433e32b7a3fa3a2a8f19a8aac73717c05cf02f
only-pine <hansol302@gmail.com>
2025-02-07 15:51:57 +0900

    Time: 0 ms (100%), Space: 48.8 MB (52.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -6,16 +6,21 @@ var climbStairs = function (n) {
     let share = Math.floor(n / 2);
     let ways = 0;
 
+    function multiply(num) {
+        let total = 1;
+        for (let index = 1; index <= num; index++) {
+            total *= index;
+        }
+
+        return total;
+    }
+
     for (let count2 = 0; count2 <= share; count2++) {
         let count1 = n - (count2 * 2);
 
-        let count1Array = Array(count1).fill().map((_, index) => index + 1);
-        let count2Array = Array(count2).fill().map((_, index) => index + 1);
-        let countArray = Array(count1 + count2).fill().map((_, index) => index + 1);
-
-        let count1Sum = count1Array.length > 0 ? count1Array.reduce((acc, cur) => acc * cur) : 1;
-        let count2Sum = count2Array.length > 0 ? count2Array.reduce((acc, cur) => acc * cur) : 1;
-        let countSum = countArray.reduce((acc, cur) => acc * cur);
+        let count1Sum = multiply(count1);
+        let count2Sum = multiply(count2);
+        let countSum = multiply(count1 + count2);
 
         ways += (countSum / (count1Sum * count2Sum));
     }

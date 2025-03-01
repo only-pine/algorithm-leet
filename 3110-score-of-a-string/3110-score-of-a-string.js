9c2ac7e9e777a1de5331ab8c361cdb1667706c4a
only-pine <hansol302@gmail.com>
2025-03-01 23:55:58 +0900

    Time: 0 ms (100%), Space: 53.8 MB (15.74%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.js
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    let code = s.charCodeAt(0);
     let sum = 0;
+    let code = s.charCodeAt(0);
 
     for (let index = 0; index < s.length; index++) {
         sum += Math.abs(code - s.charCodeAt(index));

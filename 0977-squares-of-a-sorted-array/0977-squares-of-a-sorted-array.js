29706a44d004832ab362e2d4e1b11a47b67f3844
only-pine <hansol302@gmail.com>
2025-03-14 13:03:51 +0900

    Time: 4 ms (74.87%), Space: 62.6 MB (16.65%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    // for (let index = 0; index < nums.length; index++) {
-    //     nums[index] = Math.abs(nums[index] ** 2);
-    // }
-
-    // return nums.sort((a, b) => a - b);
-
     return nums.map((num) => Math.abs(num ** 2)).sort((a, b) => a - b);
 };

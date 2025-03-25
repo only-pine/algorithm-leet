dafe730305b424e6276bee483bab7bcf7c1855d2
only-pine <hansol302@gmail.com>
2025-03-26 00:20:51 +0900

    Time: 1 ms (20.79%), Space: 55.5 MB (6.04%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (haystack.length < needle.length) {
+        return -1;
+    }
+
+    for (let index = 0; index < haystack.length; index++) {
+        if (haystack[index] === needle[0]) {
+            let subIndex = 0;
+            while (subIndex < needle.length) {
+                if (haystack[index + subIndex] === needle[subIndex]) {
+                    subIndex++;
+                    
+                    if (subIndex === needle.length) {
+                        return index;
+                    }
+                } else {
+                    subIndex = needle.length;
+                }
+            }
+        }
+    }
+
+    return -1;
+};

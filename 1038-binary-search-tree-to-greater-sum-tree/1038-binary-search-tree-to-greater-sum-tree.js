436debd8a5d58200979072d27804f4958c9a5d4c
only-pine <hansol302@gmail.com>
2025-07-12 14:48:08 +0900

    Time: 18 ms (1.69%), Space: 59.3 MB (6.78%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root, acc = 0) {
+    function plusNode (root) {
+        if (root === null) return 0;
+        return root.val + plusNode(root.left) + plusNode(root.right);
+    }
+
+    if (root === null) return null;
+
+    let sum = root.val + plusNode(root.right) + acc;
+
+    const tree = new TreeNode(sum);
+    tree.left = bstToGst(root.left, sum);
+    tree.right = bstToGst(root.right, acc);
+
+    console.log(tree);
+    return tree;
+};

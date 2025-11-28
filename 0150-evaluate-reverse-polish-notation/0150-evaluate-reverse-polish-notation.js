33e71cca7fa6352b6aff2daa5439b031d78eac44
only-pine <hansol302@gmail.com>
2025-11-28 14:50:04 +0900

    Time: 3 ms (86.6%), Space: 57.1 MB (73.93%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -3,36 +3,27 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    let index = 0;
+    const stack = [];
     const operators = ["+", "-", "*", "/"];
 
-    while (tokens[index]) {
-        if (operators.includes(tokens[index])) {
-            let answer = 0;
-            let operand_1 = Number(tokens[index - 2]);
-            let operand_2 = Number(tokens[index - 1]);
+    for (let token of tokens) {
+        if (operators.includes(token)) {
+            let b = stack.pop();
+            let a = stack.pop();
 
-            switch (tokens[index]) {
-                case "+" :
-                    answer = operand_1 + operand_2;
-                    break;
-                case "-" :
-                    answer = operand_1 - operand_2;
-                    break;
-                case "*" :
-                    answer = operand_1 * operand_2;
-                    break;
-                case "/" :
-                    answer = operand_1 / operand_2 > 0 ? Math.floor(operand_1 / operand_2) : Math.ceil(operand_1 / operand_2);
-                    break;
+            if (token === "+") {
+                stack.push(a + b);
+            } else if (token === "-") {
+                stack.push(a - b);
+            } else if (token === "*") {
+                stack.push(a * b);
+            } else {
+                stack.push(a / b > 0 ? Math.floor(a / b) : Math.ceil(a / b));
             }
-
-            tokens.splice(index - 2, 3, answer);
-            index -= 1;
         } else {
-            index += 1;
+            stack.push(parseInt(token));
         }
     }
 
-    return Number(tokens[0]);
+    return stack[0];
 };
